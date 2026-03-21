04905b5e5d5ba882de230b8ed179e23a9f90a829
Vigo Walker <vigopaul05@gmail.com>
2026-03-21 11:58:10 +0100

    Refactor code structure for improved readability and maintainability

--- frontend/app/services/WalletConnect.ts
+++ frontend/app/services/WalletConnect.ts
@@ -1,5 +1,6 @@
 import { Core } from "@walletconnect/core";
 import { WalletKit } from "@reown/walletkit";
+import { populateAuthPayload } from "@walletconnect/utils";
 
 const core = new Core({
   projectId: process.env.PROJECT_ID,
@@ -11,6 +12,74 @@ const walletKit = await WalletKit.init({
     name: "NetView",
     description: "NetView Client as Wallet/Peer",
     url: "https://netview.app/walletkit",
-    icons: [],
+    icons: ["frontend\\public\\image.png"],
   },
 });
+
+// EVM chains that your wallet supports
+const supportedChains = ["eip155:1", "eip155:2", 'eip155:137'];
+// EVM methods that your wallet supports
+const supportedMethods = ["personal_sign", "eth_sendTransaction", "eth_signTypedData"];
+// Populate the authentication payload with the supported chains and methods
+const authPayload = populateAuthPayload({
+  authPayload: payload.params.authPayload,
+  chains: supportedChains,
+  methods: supportedMethods,
+});
+// Prepare the user's address in CAIP10(https://github.com/ChainAgnostic/CAIPs/blob/main/CAIPs/caip-10.md) format
+const iss = `eip155:1:0x0Df6d2a56F90e8592B4FfEd587dB3D5F5ED9d6ef`;
+// Now you can use the authPayload to format the authentication message
+const message = walletKit.formatAuthMessage({
+  request: authPayload,
+  iss
+});
+
+const aproveSession = async (session: any) => {
+    // Approach 1
+    // Sign the authentication message(s) to create a verifiable authentication object(s)
+    const signature = await cryptoWallet.signMessage(message, privateKey);
+    // Build the authentication object(s)
+    const auth = buildAuthObject(
+    authPayload,
+    {
+        t: "eip191",
+        s: signature,
+    },
+    iss
+    );
+
+// Approve
+    await walletKit.approveSessionAuthenticate({
+    id: payload.id,
+    auths: [auth],
+    });
+
+    // Approach 2
+    // Note that you can also sign multiple messages for every requested chain/address pair
+    const auths = [];
+    authPayload.chains.forEach(async (chain) => {
+    const message = walletKit.formatAuthMessage({
+        request: authPayload,
+        iss: `${chain}:${cryptoWallet.address}`,
+    });
+    const signature = await cryptoWallet.signMessage(message);
+    const auth = buildAuthObject(
+        authPayload,
+        {
+        t: "eip191", // signature type
+        s: signature,
+        },
+        `${chain}:${cryptoWallet.address}`
+    );
+    auths.push(auth);
+    });
+
+    // Approve
+    await walletKit.approveSessionAuthenticate({
+    id: payload.id,
+    auths,
+    });
+}
+
+export default walletKit;
+export { aproveSession };
