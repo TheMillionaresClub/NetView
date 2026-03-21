1f209daef0d6e666a34c13925e1edd68f64214ce
Vigo Walker <vigopaul05@gmail.com>
2026-03-21 12:06:22 +0100

    Add WalletConnect floating widget to Home page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,6 +3,7 @@ import SideNavBar from "./components/SideNavBar";
 import BubbleMap from "./components/BubbleMap";
 import DetailsPanel from "./components/DetailsPanel";
 import BottomBar from "./components/BottomBar";
+import WalletConnectComp from "./components/WalletConnectComp";
 
 export default function Home() {
   return (
@@ -12,6 +13,10 @@ export default function Home() {
       <BubbleMap />
       <DetailsPanel />
       <BottomBar />
+      {/* WalletConnect floating widget */}
+      <div className="fixed top-16 left-24 z-50">
+        <WalletConnectComp />
+      </div>
       {/* Atmosphere Overlay */}
       <div className="fixed inset-0 pointer-events-none border-[20px] border-surface-container-lowest/20 z-[60]" />
     </>
